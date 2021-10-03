053426ec57b0fdfeca304c224bb6e390ff9d4cb9
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-10-03 03:17:50 -0700

    toggle complete

--- src/Components/Neumorphic/Toggle/Toggle.tsx
+++ src/Components/Neumorphic/Toggle/Toggle.tsx
@@ -3,16 +3,27 @@ import './Toggle.css'
 const Toggle = (props: any) => {
     const id = props.id || 1;
     const [isChecked, setChecked] = useState(props.isChecked || false);
+    const [color, setColor] = useState('#9BAACF')
     return (
-        <div className={`neumo toggle-${id}`}>
+        <div 
+        className={`neumo toggle-${id}`}
+        onMouseOver={() => {setColor('#396FF9')}}
+        onMouseOut={() => {setColor('#9BAACF')}}
+        >
             <input 
             id={`toggle-${id}`} 
             type="checkbox" 
             checked={isChecked} 
-            onClick={() => {isChecked ? setChecked(false) : setChecked(true)}} />
+            onClick={() => {isChecked ? setChecked(false) : setChecked(true)}} 
+            
+            />
             {/*@ts-ignore */}
             <label className={`toggle-label-${id}`} for={`toggle-${id}`}>
-            <i id={`icon-${id}`} className="material-icons" style={isChecked ? {color: '#396FF9'} : {color: '#9BAACF'}}>done</i></label>
+            <i id={`icon-${id}`} 
+            className="material-icons" 
+            style={isChecked ? {color: '#396FF9'} : {color}}
+            
+            >done</i></label>
         </div>
     )
 }
